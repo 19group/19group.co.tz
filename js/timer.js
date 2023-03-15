0f915f1fc9b342df3f5aeb82b463ac1b5189becc
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2023-03-15 15:44:22 +0300

    add timer function

---
+++ js/timer.js
@@ -0,0 +1,29 @@
+// Set the date and time for the countdown
+const countdownDate = new Date("2023-03-31T12:00:00").getTime();
+
+// Update the countdown every second
+const countdownInterval = setInterval(function() {
+    // Get the current date and time
+    const now = new Date().getTime();
+
+    // Calculate the time remaining
+    const timeRemaining = countdownDate - now;
+
+    // Calculate days, hours, minutes, and seconds remaining
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    // Display the countdown timer
+    document.querySelector('.days').innerHTML = days;
+    document.querySelector('.hours').innerHTML = hours;
+    document.querySelector('.minutes').innerHTML = minutes;
+    document.querySelector('.seconds').innerHTML = seconds;
+
+    // If the countdown is over, clear the interval
+    if (timeRemaining < 0) {
+        clearInterval(countdownInterval);
+        document.querySelector('.countdown').innerHTML = "The site is now ready!";
+    }
+}, 1000);
